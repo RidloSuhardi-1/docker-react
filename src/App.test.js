1f8fd93111c66907c635bb4c75e3f77e7dd1e544
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2021-12-11 23:53:11 +0700

    Update describe in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,10 @@ import App from './App';
 
 describe("<App />", () => {
   it("Renders <App /> component correctly", () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Getting started with React testing library/i)).toBeInTheDocument();
+    render(<App />);
+    expect(
+      screen.getByText(/Getting started with React testing library/i)
+    ).toBeInTheDocument();
   });
 });
 
