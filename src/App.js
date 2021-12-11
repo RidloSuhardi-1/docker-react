bcd58a8d5823de0c172a6e3ae8e1936cca35769e
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2021-12-12 00:26:45 +0700

    Update configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import { TodoList } from "./TodoList";
+import { TodoItem } from './TodoItem'
 
 function App() {
   this.setState({});
@@ -8,6 +11,12 @@ function App() {
       <header className="App-header">
         <h2>Getting started with React testing library</h2>
       </header>
+      <div className="App-body">
+        <BrowserRouter>
+          <Route path="/item/:id" component={TodoItem} />
+          <Route exact path="/" component={TodoList} />
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,12 @@
-import { render } from './custom-render';
-import App from './App';
-
+import React from "react";
+import { render, screen, waitForElementToBeRemoved } from "./custom-render";
+import App from "./App";
 describe("<App />", () => {
-  it("Renders <App /> component correctly", () => {
+  it("Renders <App /> component correctly", async () => {
     render(<App />);
     expect(
       screen.getByText(/Getting started with React testing library/i)
     ).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.getByText(/Fetching todos/i));
   });
 });
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
